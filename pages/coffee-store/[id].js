7e6f25dac422955b4cda5695e99701410ca8ee1c
muhammadmohsin123 <mohsinsurani12@gmail.com>
2022-03-03 11:02:05 +0500

    forsquare Api imolimented

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,31 +1,40 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import coffeeStoreData from "../../data/coffee-stores.json";
 import Head from "next/head";
+import styles from "../../styles/coffee-store.module.css";
+import Image from "next/image";
+import cls from "classnames";
+import axios from "axios";
+import { fectchCoffeSTores } from "../../lib/coffeSore";
 ////
+
 export async function getStaticProps(context) {
   const params = context.params;
+  const coffeeStores = await fectchCoffeSTores();
   return {
     props: {
-      coffeeStore: coffeeStoreData.find(
-        (item) => item.id.toString() === params.id
+      coffeeStore: coffeeStores.find(
+        (item) => item.fsq_id.toString() === params.id
       ),
     }, // will be passed to the page component as props
   };
 }
 /////
 export async function getStaticPaths() {
-  const paths = coffeeStoreData.map((item) => ({
-    params: { id: item.id.toString() },
+  const coffeeStores = await fectchCoffeSTores();
+  const paths = coffeeStores.map((item) => ({
+    params: { id: item.fsq_id.toString() },
   }));
   return {
     paths,
-    fallback: false, // false or 'blocking'
+    fallback: true, // false or 'blocking'
   };
 }
 
 /////
+
 function CoffeeStore(props) {
   const router = useRouter();
   // first time data needs to chached first and then serverd..so loading is needed
@@ -33,19 +42,72 @@ function CoffeeStore(props) {
   if (router.isFallback) {
     return <div>Loading....</div>;
   }
-  const { name, address, neighbourhood } = props.coffeeStore;
+  const { name, location, neighbourhood, imgUrl } = props.coffeeStore;
+  useEffect(async () => {}, []);
+  const handleUpvoteButton = () => {
+    console.log("up vote");
+  };
   return (
-    <div>
+    <div className={styles.layout}>
       <Head>
         <title>{name}</title>
       </Head>
-      <Link href='/'>
-        <a>Back to home</a>
-      </Link>
-
-      <p>{address}</p>
-      <p>{name}</p>
-      <p>{neighbourhood}</p>
+      <div className={styles.container}>
+        <div className={styles.col1}>
+          <div className={styles.backToHomeLink}>
+            <Link href='/'>
+              <a>Back to home</a>
+            </Link>
+          </div>
+          <div className={styles.nameWrapper}>
+            <h1 className={styles.name}>{name}</h1>
+          </div>
+          <Image
+            src={
+              imgUrl ||
+              "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+            }
+            width={600}
+            height={360}
+            className={styles.storeImg}
+            alt={name}
+          />
+        </div>
+        <div className={cls("glass", styles.col2)}>
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/icons/places.svg'
+              width='24'
+              height='24'
+              alt='places icon'
+            />
+            <p className={styles.text}>{location.address}</p>
+          </div>
+          {location.neighborhood.length > 0 && (
+            <div className={styles.iconWrapper}>
+              <Image
+                src='/icons/nearMe.svg'
+                width='24'
+                height='24'
+                alt='near me icon'
+              />
+              <p className={styles.text}>{location.neighborhood[0]}</p>
+            </div>
+          )}
+          <div className={styles.iconWrapper}>
+            <Image
+              src='/icons/star.svg'
+              width='24'
+              height='24'
+              alt='star icon'
+            />
+            <p className={styles.text}>3</p>
+          </div>
+          <button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+            Up vote!
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,27 @@
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Banner from "../components/banner";
 import Crad from "../components/card";
 import styles from "../styles/Home.module.css";
-import coffeeStoreData from "../data/coffee-stores.json";
+import { fectchCoffeSTores } from "../lib/coffeSore";
+import { useEffect } from "react";
+import axios from "axios";
 
 export async function getStaticProps(context) {
+  const coffeeStores = await fectchCoffeSTores();
   return {
     props: {
-      coffeeStore: coffeeStoreData,
+      coffeeStores,
     }, // will be passed to the page component as props
   };
 }
 
 export default function Home(props) {
   const handleOnBannerBtnClick = () => {
-    console.log("clicked");
+    console.log(props);
   };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -33,17 +38,20 @@ export default function Home(props) {
         <div className={styles.heroImage}>
           <Image src='/static/hero-image.png' width={700} height={400} />
         </div>
-        {props.coffeeStore.length > 0 && (
+        {props.coffeeStores?.length > 0 && (
           <>
             <h2 className={styles.heading2}>Toronto coffee stores</h2>
             <div className={styles.cardLayout}>
-              {props.coffeeStore.map((item) => {
+              {props.coffeeStores.map((item) => {
                 return (
                   <Crad
-                    key={item.id}
+                    key={item.fsq_id}
                     name={item.name}
-                    imgUrl={item.imgUrl}
-                    href={`/coffee-store/${item.id}`}
+                    imgUrl={
+                      item.imgUrl ||
+                      "https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"
+                    }
+                    href={`/coffee-store/${item.fsq_id}`}
                     className={styles.card}
                   />
                 );
